e9910375852ddaa602cd031bc189b77b4a5dc618
akriti <akritisingh1996@gmail.com>
2023-07-23 00:48:05 +0530

    'book-app'

---
+++ src/components/BookList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import BookShow from './BookShow'
+
+function BookList({onEdit, books, onDelete }) {
+  const renderedBooks = books.map(book => {
+    return <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
+  })
+  return (
+    <div className='book-list'>
+      {renderedBooks}
+    </div>
+  )
+}
+
+export default BookList
